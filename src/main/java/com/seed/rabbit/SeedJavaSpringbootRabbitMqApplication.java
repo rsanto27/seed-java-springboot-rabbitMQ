a2dfce7dae2ae7aa1773abcbf2c201c0a388507b
Rodrigo Costa Santos <rodrigocostasantos07@gmail.com>
2020-11-19 13:29:45 -0200

    starter project producer

---
+++ src/main/java/com/seed/rabbit/SeedJavaSpringbootRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.seed.rabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedJavaSpringbootRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeedJavaSpringbootRabbitMqApplication.class, args);
+	}
+
+}
